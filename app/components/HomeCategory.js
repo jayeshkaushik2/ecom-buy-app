6f56740ab73f71512eb6d88132d6b9cb8fd7901c
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-11-02 17:38:03 +0530

    fix: fixed homepage categories display

--- app/components/HomeCategory.js
+++ app/components/HomeCategory.js
@@ -1,11 +1,4 @@
-import {
-  StyleSheet,
-  Image,
-  Text,
-  View,
-  FlatList,
-  ScrollView,
-} from "react-native";
+import { Image, Text, View, FlatList, ScrollView } from "react-native";
 import React, { useState } from "react";
 import CommonStyles from "../../themes/common_style";
 
@@ -31,36 +24,40 @@ const HomeCategory = () => {
 
   return (
     <View style={styles.catContainer}>
-      <ScrollView contentContainerStyle={{ flexDirection: "column" }}>
+      <ScrollView
+        contentContainerStyle={{
+          flexDirection: "column",
+        }}
+      >
         <FlatList
+          style={{
+            padding: 10,
+            paddingTop: 0,
+            height: 95,
+            backgroundColor: "white",
+            margin: 5,
+          }}
           horizontal
-          showsHorizontalScrollIndicator={false}
+          showsHorizontalScrollIndicator={true}
           data={images}
           renderItem={({ item, index }) => (
-            <Image
-              style={styles.HomeCategory.imgContainer}
-              source={item}
-              key={index}
-            />
-          )}
-        />
-        <FlatList
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          data={[
-            { key: "1", name: "fb" },
-            { key: "2", name: "ggle" },
-            { key: "3", name: "fb" },
-            { key: "4", name: "ggle" },
-            { key: "5", name: "fb" },
-            { key: "6", name: "ggle" },
-            { key: "7", name: "fb" },
-            { key: "8", name: "ggle" },
-            { key: "9", name: "fb" },
-            { key: "10", name: "ggle" },
-          ]}
-          renderItem={({ item }) => (
-            <Text style={styles.HomeCategory.items}>{item.name}</Text>
+            <>
+              <Image
+                style={styles.HomeCategory.imgContainer}
+                source={item}
+                key={index}
+              />
+              <Text
+                style={{
+                  position: "absolute",
+                  bottom: -1,
+                  textAlign: "center",
+                  marginLeft: 4,
+                }}
+              >
+                Mobiles
+              </Text>
+            </>
           )}
         />
       </ScrollView>
