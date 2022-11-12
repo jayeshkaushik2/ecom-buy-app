41482c957108ad033b07fffa6549553709b84389
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-11-12 15:10:22 +0530

    feature: add home page API.

--- app/components/HomeImages.js
+++ app/components/HomeImages.js
@@ -1,25 +1,108 @@
 import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, ScrollView, Dimensions, Text } from "react-native";
+import { getHomepageData } from "../../context/Apis";
 
 // local imports
+import { ECOM_API_URL } from "@env";
 import CommonStyles from "../../themes/common_style";
 
 function HomeImages(props) {
+  const screenWidth = Math.round(Dimensions.get("window").width);
   const styles = CommonStyles();
-  const [currentImage, setCurrentImage] = React.useState(1);
-  const images = [
-    require("../assets/universe1.jpg"),
-    require("../assets/universe2.jpg"),
-    require("../assets/universe3.jpg"),
-  ];
+  const [HomepageData, setHomepageData] = React.useState(null);
+  const [images, setImages] = React.useState([]);
+
+  const getData = async () => {
+    try {
+      console.log("running API...");
+      const homepageData = await getHomepageData();
+      console.log(homepageData?.images);
+      setImages(homepageData?.images);
+      setHomepageData(homepageData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  React.useEffect(() => {
+    getData();
+    let interval = setInterval(() => {
+      console.log("pressed");
+    }, 5 * 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  function bottom(e) {
+    alert("Swipe Bottom");
+  }
+
+  function top(e) {
+    alert("Swipe Top");
+  }
+
+  const handleClick = (e, item) => {
+    const { swipeBottom, swipeTop } = props;
+    if (e.nativeEvent.contentOffset.y < 0) {
+      swipeBottom(item);
+    } else {
+      swipeTop(item);
+    }
+  };
 
   return (
-    <View style={{ width: "100%", height: 200 }}>
-      <Image
-        variant="image"
-        source={require(`../assets/universe${2}.jpg`)}
-        style={{ width: "100%", height: "100%" }}
-      />
+    <View
+      style={{
+        width: "100%",
+        marginTop: 10,
+        height: 200,
+        backgroundColor: "aqua",
+      }}
+    >
+      <ScrollView horizontal={true} pagingEnabled={true}>
+        {images &&
+          images.map((item, index) => (
+            <ScrollView
+              key={index}
+              onScrollEndDrag={(e) => handleClick(e, item)}
+            >
+              <Image
+                style={{ width: screenWidth, height: 200 }}
+                source={{
+                  uri: `${ECOM_API_URL}${item?.image}`,
+                }}
+              />
+              <View
+                style={{
+                  position: "absolute",
+                  top: 40,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "100%",
+                }}
+              >
+                <Text
+                  style={{
+                    color: item?.is_dark === true ? "white" : "black",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {HomepageData?.title}
+                </Text>
+                <Text
+                  style={{
+                    color: item?.is_dark === true ? "white" : "black",
+                    top: 10,
+                    textAlign: "center",
+                  }}
+                >
+                  {HomepageData?.description.slice(0, 150)}...
+                </Text>
+              </View>
+            </ScrollView>
+          ))}
+      </ScrollView>
     </View>
   );
 }
